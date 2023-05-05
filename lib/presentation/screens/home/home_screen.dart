dad5ce0122ecc853906a666f0cf5cbceb94aebac
Andres Silva <silvaescobarandres@gmail.com>
2023-05-05 18:04:20 -0400

    rutas basicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +11,52 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final List<MenuItem> menuItems = appMenuItems;
+
+    return _CustomListTile(menuItems: menuItems);
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.menuItems,
+  });
+
+  final List<MenuItem> menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListView.builder(
+        itemCount: menuItems.length,
+        itemBuilder: (context, i) {
+          return ListTile(
+            leading: Icon(
+              menuItems[i].icon,
+              color: colors.primary,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: colors.primary,
+            ),
+            title: Text(menuItems[i].title),
+            subtitle: Text(menuItems[i].subTitle),
+            onTap: () {
+              Navigator.pushNamed(context, menuItems[i].link);
+            },
+          );
+        });
   }
 }
